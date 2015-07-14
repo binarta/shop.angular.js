4e9b31c21b81f0003ed1910d9113139beba7e00d
wro <wro@thinkerit.be>
2015-07-14 11:58:43 +0200

    added order management routes

--- src/shop.js
+++ src/shop.js
@@ -19,6 +19,9 @@ angular.module('shop', [
         .when('/profile', {redirectTo: '/account'})
         .when('/summary', {templateUrl: 'partials/shop/summary.html'})
         .when('/order-confirmation', {templateUrl: 'partials/shop/order-confirmation.html'})
+        .when('/order-history', {templateUrl: 'partials/shop/history.html', controller: 'ListPurchaseOrderController'})
+        .when('/admin/orders', {templateUrl: 'partials/shop/clerk/history.html', controller: 'ListPurchaseOrderController'})
+        .when('/admin/order/:owner/:id', {templateUrl: 'partials/shop/order-details.html', controller: 'ViewPurchaseOrderController'})
         .when('/:locale/basket', {templateUrl: 'partials/shop/basket.html'})
         .when('/:locale/checkout/address', {templateUrl: 'partials/shop/order-address.html'})
         .when('/:locale/address/add', {templateUrl: 'partials/address/add-address.html', controller: 'CustomerAddressController'})
@@ -27,4 +30,7 @@ angular.module('shop', [
         .when('/:locale/profile', {redirectTo: '/account'})
         .when('/:locale/summary', {templateUrl: 'partials/shop/summary.html'})
         .when('/:locale/order-confirmation', {templateUrl: 'partials/shop/order-confirmation.html'})
+        .when('/:locale/order-history', {templateUrl: 'partials/shop/history.html', controller: 'ListPurchaseOrderController'})
+        .when('/:locale/admin/orders', {templateUrl: 'partials/shop/clerk/history.html', controller: 'ListPurchaseOrderController'})
+        .when('/:locale/admin/order/:owner/:id', {templateUrl: 'partials/shop/order-details.html', controller: 'ViewPurchaseOrderController'})
 }]);
