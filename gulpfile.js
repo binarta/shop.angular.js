c1d80c15d524a8a98f62330769ba92f006c5af8a
Jo Kerkhofs <jke@thinkerit.be>
2016-02-01 16:52:16 +0100

    Removed the paypal flag (this is now part of the shop flag)

--- gulpfile.js
+++ gulpfile.js
@@ -19,12 +19,4 @@ gulp.task('shop-bootstrap3', function () {
         .pipe(gulp.dest('src'));
 });
 
-gulp.task('paypal-bootstrap3', function () {
-    gulp.src('template/bootstrap3/*.html')
-        .pipe(template({paypal: true}))
-        .pipe(minifyHtml(minifyHtmlOpts))
-        .pipe(templateCache('paypal-tpls-bootstrap3.js', {standalone: true, module: 'shop.templates'}))
-        .pipe(gulp.dest('src'));
-});
-
-gulp.task('default', ['shop-bootstrap3', 'paypal-bootstrap3']);
+gulp.task('default', ['shop-bootstrap3']);
