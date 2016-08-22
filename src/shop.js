a04a5dda89f70eed7019521819deeecebcc5c9ea
sgo <sgo@thinkerit.be>
2016-08-22 16:20:23 +0200

    removing dependency on basket.js

--- src/shop.js
+++ src/shop.js
@@ -1,6 +1,5 @@
 angular.module('shop', [
     'professional.app',
-    'basket',
     'purchase.orders',
     'customer.address',
     'customer.address.templates',
@@ -9,22 +8,16 @@ angular.module('shop', [
     'shop.templates'
 ]).config(['$routeProvider', function($routeProvider) {
     $routeProvider
-        // .when('/checkout/address', {templateUrl: 'partials/shop/order-address.html'})
         .when('/address/add', {templateUrl: 'partials/address/add-address.html'})
         .when('/address/:label', {templateUrl: 'partials/address/edit-address.html'})
         .when('/account', {templateUrl: 'partials/account.html'})
         .when('/profile', {redirectTo: '/account'})
-        // .when('/summary', {templateUrl: 'partials/shop/summary.html'})
-        // .when('/order-confirmation', {templateUrl: 'partials/shop/order-confirmation.html'})
         .when('/order-history', {templateUrl: 'partials/shop/order-history.html', controller: 'BinartaSearchController as searchCtrl'})
         .when('/order/:owner/:id', {templateUrl: 'partials/shop/order-details.html', controller: 'ViewPurchaseOrderController as viewOrderCtrl'})
-        // .when('/:locale/checkout/address', {templateUrl: 'partials/shop/order-address.html'})
         .when('/:locale/address/add', {templateUrl: 'partials/address/add-address.html'})
         .when('/:locale/address/:label', {templateUrl: 'partials/address/edit-address.html'})
         .when('/:locale/account', {templateUrl: 'partials/account.html'})
         .when('/:locale/profile', {redirectTo: '/account'})
-        // .when('/:locale/summary', {templateUrl: 'partials/shop/summary.html'})
-        // .when('/:locale/order-confirmation', {templateUrl: 'partials/shop/order-confirmation.html'})
         .when('/:locale/order-history', {templateUrl: 'partials/shop/order-history.html', controller: 'BinartaSearchController as searchCtrl'})
         .when('/:locale/order/:owner/:id', {templateUrl: 'partials/shop/order-details.html', controller: 'ViewPurchaseOrderController as viewOrderCtrl'})
 }]);
